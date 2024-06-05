dc0dd6b28dd3a234141eff2d1f54256fab9eb06f
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-06-06 00:56:46 +0530

    added font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+
+  useFonts({
+    'poppins':require("../assets/fonts/Poppins-Regular.ttf"),
+    'poppins-med':require("../assets/fonts/Poppins-Medium.ttf"),
+    'poppins-bold':require("../assets/fonts/Poppins-Bold.ttf")
+  })
+
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       {/* <Stack.Screen name="index" /> */}
