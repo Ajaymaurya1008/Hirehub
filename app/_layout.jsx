87d008cda5f275b81fde2816753c3344c25ae38b
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-06-06 20:44:26 +0530

    added authentication

---
+++ app/_layout.jsx
@@ -0,0 +1,51 @@
+import { SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import SignInWithOAuth from "@/components/SignInWithOAuth";
+
+
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+
+
+export default function RootLayout() {
+  useFonts({
+    poppins: require("../assets/fonts/Poppins-Regular.ttf"),
+    "poppins-med": require("../assets/fonts/Poppins-Medium.ttf"),
+    "poppins-bold": require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <SignedIn>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <SignInWithOAuth />
+      </SignedOut>
+    </ClerkProvider>
+  );
+}
